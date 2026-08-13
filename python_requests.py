84b9c25d9adcec127aa4d8f94cfb8364e839f7bc
MansoorAhmadQazi <mansoorqazi71@gmail.com>
2026-08-13 22:11:29 +0500

    request package and python functions

---
+++ python_requests.py
@@ -0,0 +1,14 @@
+# import requests
+
+# # Download a web page
+# response = requests.get("https://api.github.com")
+# print(response.status_code)  # Should print 200
+
+def say_goodbye():
+    print("Goodbye!")
+    print("See you later!")
+
+# Call it multiple times
+say_goodbye()
+say_goodbye()
+say_goodbye()
